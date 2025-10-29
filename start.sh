ba2be86d020a1cb98a63c91251af6c29c8a4ae0e
Gabriel Carvalho <gabriel.carvalho@grupoigd.com.br>
2025-10-29 11:45:48 -0300

    Chore - Adicionar exemplo de env e ajustar rotina de inicialização
    Inclui arquivo de exemplo para variáveis de ambiente facilitando configuração.
    Melhora script de start para aplicar migrações apenas se existirem ou sincronizar schema,
    aumentando a robustez e automatização da inicialização do back-end.

--- start.sh
+++ start.sh
@@ -6,14 +6,21 @@ until nc -z database 5432; do
   sleep 5
 done
 
+export CI=${CI:-true}
+
 echo "database disponível. Instalando dependências"
-pnpm install
+pnpm install --frozen-lockfile --force
 
 echo "Gerando schema prisma"
 pnpm generate
 
-echo "Executando migration do banco de dados..."
-pnpm db:deploy
+if [ -d "prisma/migrations" ] && [ "$(find prisma/migrations -mindepth 1 -maxdepth 1 -type d | wc -l)" -gt 0 ]; then
+  echo "Executando migration do banco de dados..."
+  pnpm db:deploy
+else
+  echo "Sincronizando schema com o banco de dados (prisma db push)..."
+  pnpm prisma db push
+fi
 
 echo "Executando seed do banco de dados..."
 pnpm db:seed
